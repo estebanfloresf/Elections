897ffea61378fa6d8c8a2de46c67677c11f6b181
Esteban <esteban_fs@hotmail.com>
2017-06-26 09:48:10 -0500

    Fxes for starting the project

--- helpers.js
+++ helpers.js
@@ -23,6 +23,7 @@ exports.siteName = `Elecciones`;
 exports.menu = [
   { slug: '/surveys', title: 'Surveys', icon: 'survey', },
     { slug: '/candidates', title: 'Candidates', icon: 'candidates', },
+    { slug: '/dashboard', title: 'Dashboard', icon: 'dashboard', },
   // { slug: '/tags', title: 'Tags', icon: 'tag', },
   // { slug: '/top', title: 'Top', icon: 'top', },
   // { slug: '/add', title: 'Add', icon: 'add', },
