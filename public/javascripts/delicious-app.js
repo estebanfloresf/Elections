f4eaa7b4d9dbccd1e026644355de5b9234d43287
Esteban <esteban_fs@hotmail.com>
2017-07-28 18:37:31 -0500

    Animation Sort done

--- public/javascripts/delicious-app.js
+++ public/javascripts/delicious-app.js
@@ -1,21 +1,15 @@
 import '../sass/style.scss';
 
-import { $, $$ } from './modules/bling';
+import {$, $$} from './modules/bling';
 
 import inlinesvg from './modules/inline_svg';
 
-import orderBy from './modules/orderBy';
+
 
 inlinesvg($('.active  img.svg'));
 
-orderBy($('#media'));
 
 
-$(function() {
-    $("button").click(function() {
-        animateSort("#candidate", "#main", "data-value");
-    });
-});
 
 
 
--- public/javascripts/modules/orderBy.js
+++ public/javascripts/modules/orderBy.js
@@ -1,42 +1,79 @@
+$(document).ready(function() {
+    $("#percentage").first().addClass("active");
+    $("button").click(function () {
+        $("button").removeClass("active");
+
+        $(this).addClass("active");
+
+        let value = "";
 
 
-function orderBy(e) {
+        if (this.id == "percentage"){
+            value = "data-value";
+        }
+        else{
+            value = "data-name";
+        }
 
 
-        var promises = [];
-        var positions = [];
-        var originals = $(parent).find(child);
-        var sorted = originals.toArray().sort(function(a, b) {
-            return $(a).attr(sortAttribute) > $(b).attr(sortAttribute);
+
+        animateSort(".candidate", "div.media", value);
+
+
+    });
+
+
+
+
+});
+
+function animateSort(parent, child, sortAttribute) {
+
+
+    const promises = [];
+    const positions = [];
+    const originals = $(parent).find(child);
+    let sorted = {};
+
+    if(sortAttribute=="data-name"){
+
+        sorted = originals.toArray().sort(function (a,b) {
+            const aName = $(a).attr(sortAttribute).toLowerCase();
+            const bName = $(b).attr(sortAttribute).toLowerCase();
+            return ((aName < bName) ? -1 : ((aName > bName) ? 1 : 0));
         });
+    }
+    else {
 
-        originals.each(function() {
-            //store original positions
-            positions.push($(this).position());
-        }).each(function(originalIndex) {
-            //change items to absolute position
-            var $this = $(this);
-            var newIndex = sorted.indexOf(this);
-            sorted[newIndex] = $this.clone(); //copy the original item before messing with its positioning
-            $this.css("position", "absolute").css("top", positions[originalIndex].top + "px").css("left", positions[originalIndex].left + "px");
+        sorted = originals.toArray().sort(function (a, b) {
 
-            //animate to the new position
-            var promise = $this.animate({
-                top: positions[newIndex].top + "px",
-                left: positions[newIndex].left + "px"
-            }, 1000);
-            promises.push(promise);
+            return $(b).attr(sortAttribute) - $(a).attr(sortAttribute);
         });
+    }
 
-        //instead of leaving the items out-of-order and positioned, replace them in sorted order
-        $.when.apply($, promises).done(function() {
-            originals.each(function(index) {
-                $(this).replaceWith(sorted[index]);
-            });
+    originals.each(function() {
+        //store original positions
+        positions.push($(this).position());
+    }).each(function(originalIndex) {
+        //change items to absolute position
+        const $this = $(this);
+        const newIndex = sorted.indexOf(this);
+        sorted[newIndex] = $this.clone(); //copy the original item before messing with its positioning
+        $this.css("position", "absolute").css("top", positions[originalIndex].top + "px").css("left", positions[originalIndex].left + "px");
+
+        //animate to the new position
+        const promise = $this.animate({
+            top: positions[newIndex].top + "px",
+            left: positions[newIndex].left + "px"
+        }, 1000);
+        promises.push(promise);
+    });
+
+    //instead of leaving the items out-of-order and positioned, replace them in sorted order
+    $.when.apply($, promises).done(function() {
+        originals.each(function(index) {
+            $(this).replaceWith(sorted[index]);
         });
-
-
-
+    });
 }
 
-export default orderBy;
