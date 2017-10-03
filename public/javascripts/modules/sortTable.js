8f5eba802eeb8abec849d385ce612f6645d1990c
Esteban <esteban_fs@hotmail.com>
2017-10-03 16:36:44 -0500

    Fixed table issues

--- public/javascripts/modules/sortTable.js
+++ public/javascripts/modules/sortTable.js
@@ -1,91 +1,29 @@
-function sortTable(n, id) {
+//
+// I came across this, and thought I'd throw in my 2 cents. Click on the column headers to sort ascending, and again to sort descending.
+//
+// Works in Chrome, Firefox, Opera AND IE(8)
+// Only uses JQuery
+// Does alpha and numeric sorting - ascending and descending
+$('th').click(function () {
+    var table = $(this).parents('table').eq(0);
 
-
-    var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
-    table = id;
-
-    /*Loop through all table rows (except the
-    first, which contains table headers):*/
-    switching = true;
-    //Set the sorting direction to ascending:
-    dir = "asc";
-    /*Make a loop that will continue until
-    no switching has been done:*/
-    // while (switching) {
-    //     //start by saying: no switching is done:
-    //     switching = false;
-    rows = table.getElementsByTagName("tr");
-    while (switchcount < rows.length - 1) {
-
-        for (i = 1; i < (rows.length - 1); i++) {
-            //start by saying there should be no switching:
-            shouldSwitch = false;
-            /*Get the two elements you want to compare,
-            one from current row and one from the next:*/
-
-
-            x = rows[i].getElementsByTagName("td")[n];
-            y = rows[i + 1].getElementsByTagName("td")[n];
-
-            if (x.innerHTML > y.innerHTML) {
-                shouldSwitch = true;
-            }else {
-                switchcount++;
-            }
-
-            if (shouldSwitch) {
-                rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-                switchcount++;
-            }
-
-
-            //
-            //         /*check if the two rows should switch place,
-            //         based on the direction, asc or desc:*/
-            //         if (dir === "asc") {
-            //
-            //             if(n == 1){
-            //                 x.innerHTML - y.innerHTML
-            //                 shouldSwitch = true;
-            //                 break;
-            //
-            //
-            //             }
-            //
-            //
-            //
-            //             if ( x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
-            //                 //if so, mark as a switch and break the loop:
-            //
-            //                 shouldSwitch = true;
-            //                 break;
-            //             }
-            //         } else if (dir === "desc") {
-            //             if ( x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
-            //                 //if so, mark as a switch and break the loop:
-            //                 shouldSwitch = true;
-            //                 break;
-            //             }
-            //         }
-            //     }
-            //
-            //     if (shouldSwitch) {
-            //         /*If a switch has been marked, make the switch
-            //         and mark that a switch has been done:*/
-            //         rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-            //         switching = true;
-            //         //Each time a switch is done, increase this count by 1:
-            //         switchcount++;
-            //     } else {
-            //         /*If no switching has been done AND the direction is "asc",
-            //         set the direction to "desc" and run the while loop again.*/
-            //         if (switchcount == 0 && dir == "asc") {
-            //             dir = "desc";
-            //             switching = true;
-            //         }
-            //     }
-            // }
-        }
+    var rows = table.find('tr:gt(0)').toArray().sort(comparer($(this).index()));
+    this.asc = !this.asc;
+    if (!this.asc) {
+        rows = rows.reverse()
     }
+    for (var i = 0; i < rows.length; i++) {
+        table.append(rows[i])
+    }
+});
 
+function comparer(index) {
+    return function (a, b) {
+        var valA = getCellValue(a, index), valB = getCellValue(b, index)
+        return $.isNumeric(valA) && $.isNumeric(valB) ? valA - valB : valA.localeCompare(valB)
+    }
+}
+
+function getCellValue(row, index) {
+    return $(row).children('td').eq(index).text()
 }
